a1223881969d00a53ac136a146da3e714fdec8ac
DardanJ <99636674+DardanJ@users.noreply.github.com>
2026-08-23 20:10:14 +1000

    feat: set up routing for booking page

---
+++ client/src/Booking.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+
+function Booking() {
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Booking;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Booking from "./Booking.jsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/Booking" element={<Booking />} />
+    </Routes>
+  </BrowserRouter>,
+);
