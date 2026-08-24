19bd69fe48f645fdd503ffe4f29a7ccac2445ccf
DardanJ <99636674+DardanJ@users.noreply.github.com>
2026-08-24 22:10:55 +1000

    feat: booking page setup

--- client/src/Booking.jsx
+++ client/src/Booking.jsx
@@ -12,6 +12,33 @@ function Booking() {
           <p>Home Page</p>
         </NavLink>
       </section>
+
+      <section id="next-steps">
+        <div>
+          <h2>Select Date</h2>
+          <div>
+            <strong>Court 1: </strong>
+            <button>9:00am</button>
+            <button>10:00am</button>
+            <button>11:00am</button>
+            <button>12:00pm</button>
+            <button>1:00pm</button>
+          </div>
+        </div>
+      </section>
+      <section id="next-steps">
+        <div>
+          <h2>Select Date</h2>
+          <div>
+            <strong>Court 2: </strong>
+            <button>9:00am</button>
+            <button>10:00am</button>
+            <button>11:00am</button>
+            <button>12:00pm</button>
+            <button>1:00pm</button>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
